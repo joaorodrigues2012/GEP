4d558e9bdd6ba9611ff1c598385fe6352c40dbc8
shalom fernando <shalonando@gmail.com>
2019-10-24 20:52:35 -0300

    Salvar Cronograma

--- src/app/pages/projetos/projetos.component.ts
+++ src/app/pages/projetos/projetos.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Projetos} from '../../model/projetos';
 import {ProjetoService} from '../../service/projeto.service';
 import {MessageService} from 'primeng/api';
+import {Cronograma} from '../../model/Cronograma';
 
 @Component({
   selector: 'app-projetos',
@@ -14,6 +15,14 @@ export class ProjetosComponent implements OnInit {
 
   colunas: any[];
   projeto: Projetos;
+
+  displayDialog: boolean;
+  editProjetos: any;
+  selectedCronograma: Projetos;
+  newProjeto: boolean;
+  projetos: Projetos;
+
+
   constructor(private projetoService: ProjetoService, private messageService: MessageService) {
   }
 
@@ -27,4 +36,67 @@ export class ProjetosComponent implements OnInit {
       {field: 'objetivo', header: 'Objetivo'}
     ];
   }
+
+  // salvarCronograma(cronograma: any) {
+  //   // this.cronogramaService.POST(cronograma).subscribe();
+  //   console.log(cronograma, 'salvo');
+  // }
+  //
+  // atualizarCronograma(cronograma: any) {
+  //   this.cronogramaService.PUT(cronograma, this.editCronograma.id).subscribe();
+  // }
+
+  showDialogToAdd() {
+    this.newProjeto = true;
+    this.editProjetos = {};
+    this.displayDialog = true;
+  }
+
+  save() {
+    let proje = [...this.listaProjetos];
+
+    console.log(this.newProjeto);
+
+    if (this.newProjeto) {
+      proje.push(this.editProjetos);
+      // this.salvarCronograma(this.cronogramaSalve.conversor(this.editProjetos));
+    } else {
+      proje[this.listaProjetos.indexOf(this.projetos)] = this.editProjetos;
+      // this.atualizarCronograma(this.cronogramaAtualizar.converter(this.editProjetos));
+      console.log(this.projetos, 'aqui');
+
+    }
+
+    this.listaProjetos = proje;
+    this.editProjetos = null;
+    this.displayDialog = false;
+  }
+
+  delete() {
+
+    let index = this.listaProjetos.indexOf(this.projetos);
+    this.listaProjetos = this.listaProjetos.filter((val, i) => i != index);
+    this.editProjetos = null;
+    this.displayDialog = false;
+
+    // this.cronogramaService.deletarCronograma(this.projetos.id);
+
+  }
+
+  onRowSelect(event) {
+    this.newProjeto = false;
+    this.editProjetos = this.cloneProjeto(event.data);
+    console.log(this.editProjetos, 'onrow');
+    this.displayDialog = true;
+  }
+
+  //// Pega a linha clicada e inputa no editProjeto
+  cloneProjeto(c: Projetos): Projetos {
+    let proje = {};
+    for (let prop in c) {
+      proje[prop] = c[prop];
+    }
+    return <Projetos>proje;
+  }
+
 }
--- src/app/pages/tarefa/tarefa.component.ts
+++ src/app/pages/tarefa/tarefa.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Tarefa} from '../../model/tarefa';
 import {TarefaService} from '../../service/tarefa.service';
+import {Projetos} from '../../model/projetos';
 
 @Component({
   selector: 'app-tarefa',
@@ -10,13 +11,74 @@ import {TarefaService} from '../../service/tarefa.service';
 export class TarefaComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
 
-  constructor(tarefaService: TarefaService) {
-    tarefaService.listar().subscribe(listaTarefas => {
-      this.listaTarefas = listaTarefas;
-    });
-  }
+  colunas :any[];
+  tarefa :Tarefa;
+
+  displayDialog: boolean;
+  editTarefas: any;
+  newTarefa: boolean;
+
+
+  constructor(private tarefaService: TarefaService) {}
 
   ngOnInit() {
+    this.tarefaService.listar().subscribe(listaTarefas => {
+      this.listaTarefas = listaTarefas;
+    });
+    this.colunas = [
+      {field: 'descricao', header: 'Descricao'},
+      {field: 'dias', header: 'Dias'},
+      {field: 'dt_INICIO', header: 'Data Inicio'},
+      {field: 'dt_FIM', header: 'Data Fim'},
+      {field: 'responsavel', header: 'Responsavel'},
+      {field: 'status', header: 'Status'}
+    ];
   }
 
+
+  showDialogToAdd() {
+    this.editTarefas = {};
+    this.newTarefa = true;
+    this.displayDialog = true;
+  }
+
+  save(){
+    let taref = [...this.listaTarefas];
+
+    if (this.newTarefa){
+      taref.push(this.editTarefas);
+    }
+    else {
+      taref[this.listaTarefas.indexOf(this.tarefa)] = this.editTarefas;
+    }
+
+    this.listaTarefas = taref;
+    this.editTarefas = null;
+    this.displayDialog = false;
+  }
+
+  delete() {
+
+    let index = this.listaTarefas.indexOf(this.tarefa);
+    this.listaTarefas = this.listaTarefas.filter((val, i) => i != index);
+    this.editTarefas = null;
+    this.displayDialog = false;
+
+    // this.cronogramaService.deletarCronograma(this.projetos.id);
+
+  }
+
+
+  onRowSelect($event: any) {
+    this.newTarefa = false;
+    this.editTarefas = this.cloneTarefa($event);
+    this.displayDialog = true;
+  }
+  cloneTarefa(c: Tarefa): Tarefa {
+    let taref = {};
+    for (let prop in c) {
+      taref[prop] = c[prop];
+    }
+    return <Tarefa>taref;
+  }
 }
